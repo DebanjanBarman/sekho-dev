a5329d21271cfcfe3797b51d2973d78480ea0813
DebanjanBarman <debanjanbarman123456789@gmail.com>
2020-08-20 21:37:06 +0530

    production duplicate email fixing

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -7,8 +7,7 @@ const handleCastErrorDB = err => {
 
 const handleDuplicateFieldsDB = err => {
 
-    console.log(err)
-    const value = err.keyValue
+    const value = err.keyValue.email
 
     const message = `Duplicate field value: ${value}. Please use another value!`;
     return new AppError(message, 400);
